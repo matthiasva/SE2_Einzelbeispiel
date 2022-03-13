b54e747a381811acae129d029b6e21aeddecb188
matthiasva <matthiasva@edu.aau.at>
2022-03-13 18:17:00 +0100

    Einzelbeispiel_2.1

--- app/src/main/java/com/example/einzelbeispielse2/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispielse2/MainActivity.java
@@ -3,27 +3,19 @@ package com.example.einzelbeispielse2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView textView2;
     TextView textView;
-    Button button;
+    EditText editText;
+    Button buttonSend;
+    Button buttonCalculate;
     TCPClient tcpClient;
 
     @Override
@@ -31,8 +23,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-        button.setOnClickListener(this);
+        buttonSend = findViewById(R.id.button);
+        buttonSend.setOnClickListener(this);
+
+        buttonCalculate = findViewById(R.id.button2);
+        buttonCalculate.setOnClickListener(this);
+
+        editText = findViewById(R.id.editTextNumber);
+        textView2 = findViewById(R.id.textView2);
+
 
         tcpClient = new TCPClient();
         tcpClient.execute();
@@ -40,12 +39,163 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        textView = findViewById(R.id.textView);
-        textView2 = findViewById(R.id.textView2);
+        if (view.getId() == R.id.button) {
+            editText.setText(tcpClient.message);
+            textView2.setText(tcpClient.modifiedMessage);
+        }
+        if (view.getId() == R.id.button2) {
+            int matNr = Integer.parseInt(editText.getText().toString());
+            String n = editText.getText().toString();
+            int matNrMod = matNr % 7;
+            String matNrModString = String.valueOf(matNrMod);
+            textView = findViewById(R.id.textView3);
+            textView.setText("% 7 = "+matNrModString);
 
-        textView.setText(tcpClient.message);
-        textView2.setText(tcpClient.modifiedMessage);
+            String temp = Integer.toString(matNr);
+            String[] arrayString = new String[temp.length()];
+            for (int i = 0; i < temp.length(); i++) {
+                arrayString[i] = String.valueOf(temp.charAt(i));
+            }
+            int[] arrayInt = new int[temp.length()];
+            for (int i = 0; i < temp.length(); i++) {
+                arrayInt[i] = Integer.parseInt(arrayString[i]);
+            }
+            int countEven = 0;
+            int countOdd = 0;
+            for (int i = 0; i < arrayString.length; i++) {
+                if (Integer.parseInt(arrayString[i]) % 2 == 0) {
+                    countEven++;
+                }else {
+                    countOdd++;
+                }
+            }
+
+            String[] arrayEven = new String[countEven];
+            String[] arrayOdd = new String[countOdd];
+            int j = 0;
+            int k = 0;
+            for (int i = 0; i < temp.length(); i++) {
+                if (Integer.parseInt(arrayString[i]) % 2 == 0) {
+                    arrayEven[j] = String.valueOf(temp.charAt(i));
+                    j++;
+                }else {
+                    arrayOdd[k] = String.valueOf(temp.charAt(i));
+                    k++;
+                }
+            }
+
+            switch (matNrMod) {
+
+                case 0:
+                    int sum = 0;
+                    for(int i = 0; i<n.length(); i++) {
+                        if(i%2==0)
+                            sum+=Integer.parseInt(String.valueOf(n.charAt(i)));
+                        else
+                            sum-=Integer.parseInt(String.valueOf(n.charAt(i)));
+                    }
+                    if (sum % 2 == 0) {
+                        String output = String.valueOf(sum);
+                        textView2.setText(output+"\nGerade");
+                    }else {
+                        String output = String.valueOf(sum);
+                        textView2.setText(output+"\nUngerade");
+                    }
+                    break;
+
+                case 1:
+                    Arrays.sort(arrayEven);
+                    Arrays.sort(arrayOdd);
+                    textView2.setText(Arrays.toString(arrayEven)+"\n"+Arrays.toString(arrayOdd));
+                    break;
+
+                case 2:
+                    int[] arrayTeiler = new int[2];
+                    for (int i = 0; i < arrayInt.length; i++) {
+                        for (int x = 0; x < arrayInt.length; x++) {
+                            for (int y = 2; y < 10; y++) {
+                            if ((arrayInt[i] % y == 0) && (arrayInt[x] % y == 0) && (i != x)) {
+                                arrayTeiler[0] = x;
+                                arrayTeiler[1] = i;
+                            }
+                        }
+                        }
+                    }
+                    textView2.setText(Arrays.toString(arrayTeiler));
+                    break;
+
+                case 3:
+                    char[] arrayChar = new char[temp.length()];
+                    for (int i = 0; i < arrayInt.length; i++) {
+                        if (i % 2 != 0) {
+                            arrayChar[i] = (char) (arrayInt[i]+96);
+                        }else {
+                            arrayChar[i] = (char) (arrayInt[i]+48);
+                        }
+                    }
+                    textView2.setText(Arrays.toString(arrayChar));
+                    break;
+
+                case 4:
+                    int quersumme = 0;
+                    String binary = "";
+                    for (int i = 0; i < temp.length(); i++) {
+                        quersumme += arrayInt[i];
+                    }
+                    binary = Integer.toBinaryString(quersumme);
+                    textView2.setText("Quersumme: "+String.valueOf(quersumme)+"\nBinär: "+binary);
+                    break;
+
+                case 5:
+                    Arrays.sort(arrayInt);
+                    int count = 0;
+                    for (int i = 0; i < arrayInt.length; i++) {
+                        if (!isPrime(arrayInt[i])) {
+                            count++;
+                        }
+                    }
+                    int[] array = new int[count];
+                    int x = 0;
+                    for (int i = 0; i < arrayInt.length; i++) {
+                        if (!isPrime(arrayInt[i])) {
+                            array[x] = arrayInt[i];
+                            x++;
+                        }
+                    }
+                    textView2.setText(Arrays.toString(array));
+                    break;
+
+                case 6:
+                    Arrays.sort(arrayInt);
+                    int countPrime = 0;
+                    for (int i = 0; i < arrayInt.length; i++) {
+                        if (isPrime(arrayInt[i])) {
+                            countPrime++;
+                        }
+                    }
+
+                    int[] arrayPrime = new int[countPrime];
+                    int y = 0;
+                    for (int i = 0; i < arrayInt.length; i++) {
+                        if (isPrime(arrayInt[i])) {
+                            arrayPrime[y] = arrayInt[i];
+                            y++;
+                        }
+                    }
+                    textView2.setText(Arrays.toString(arrayPrime));
+            }
+
+        }
     }
 
-
+    public static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
 }
+
+
--- app/src/main/java/com/example/einzelbeispielse2/TCPClient.java
+++ app/src/main/java/com/example/einzelbeispielse2/TCPClient.java
@@ -1,19 +1,14 @@
 package com.example.einzelbeispielse2;
 
-import android.location.OnNmeaMessageListener;
 import android.os.AsyncTask;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.nio.Buffer;
+
 
 public class TCPClient extends AsyncTask<String,Void,Void> {
 
@@ -37,21 +32,11 @@ public class TCPClient extends AsyncTask<String,Void,Void> {
             message = inFromUser.readLine();
             outToServer.writeBytes(message + "\n");
             modifiedMessage = inFromServer.readLine();
-
-            System.out.println("From Server: " + modifiedMessage);
             socket.close();
+
         }catch (IOException e) {
             System.out.println("IOException");
         }
         return null;
-    }
-
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getModifiedMessage() {
-        return modifiedMessage;
     }
 }
